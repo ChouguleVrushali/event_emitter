675ca91d2defd84fa08f527e3c09e1c4c3515064
ChouguleVrushali <vrushalichougule2505@gmail.com>
2024-09-30 14:56:20 +0530

    Add files via upload

--- emitter.js
+++ emitter.js
@@ -1,123 +1,123 @@
 /*..................Registering & Emitting Events............*/
 
-// import EventEmitter from 'events';
-// const eventEmitterObj=new EventEmitter();
+import EventEmitter from 'events';
+const eventEmitterObj=new EventEmitter();
 
-// eventEmitterObj.setMaxListeners(15);
-// eventEmitterObj.addListener('msg',(data)=>{
-//     console.log(data);
-// })
-// eventEmitterObj.addListener('msg',(data)=>{
-//     console.log(data);
-// })
+eventEmitterObj.setMaxListeners(15);
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
 
-// eventEmitterObj.addListener('msg',(data)=>{
-//     console.log(data);
-// })
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
 
-// eventEmitterObj.addListener('msg',(data)=>{
-//     console.log(data);
-// })
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
 
-// eventEmitterObj.addListener('msg',(data)=>{
-//     console.log(data);
-// })
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
 
-// eventEmitterObj.addListener('msg',(data)=>{
-//     console.log(data);
-// })
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
 
-// eventEmitterObj.addListener('msg',(data)=>{
-//     console.log(data);
-// })
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
 
-// eventEmitterObj.addListener('msg',(data)=>{
-//     console.log(data);
-// })
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
 
-// eventEmitterObj.addListener('msg',(data)=>{
-//     console.log(data);
-// })
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
 
-// eventEmitterObj.addListener('msg',(data)=>{
-//     console.log(data);
-// })
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
 
-// eventEmitterObj.on('end',(data)=>{
-//     console.log(data);
-// })
+eventEmitterObj.on('end',(data)=>{
+    console.log(data);
+})
 
-// eventEmitterObj.once('start',(data)=>{
-//     console.log(data);
-// })
+eventEmitterObj.once('start',(data)=>{
+    console.log(data);
+})
 
-// eventEmitterObj.addListener('msg',(data)=>{
-//     console.log(data);
-// })
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
 
-// eventEmitterObj.addListener('msg',(data)=>{
-//     console.log(data);
-// })
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
 
-// eventEmitterObj.addListener('msg',(data)=>{
-//     console.log(data);
-// })
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
 
 
-// eventEmitterObj.emit('msg','hii msg event 1');
-// eventEmitterObj.emit('msg');
-// eventEmitterObj.emit('end','hii end event 1');
-// eventEmitterObj.emit('start','hii start event 1');
-// eventEmitterObj.emit('start','hii start event 2');
-// eventEmitterObj.emit('end','hii end event 2');
-// eventEmitterObj.emit('start','hii start event 3');
+eventEmitterObj.emit('msg','hii msg event 1');
+eventEmitterObj.emit('msg');
+eventEmitterObj.emit('end','hii end event 1');
+eventEmitterObj.emit('start','hii start event 1');
+eventEmitterObj.emit('start','hii start event 2');
+eventEmitterObj.emit('end','hii end event 2');
+eventEmitterObj.emit('start','hii start event 3');
 
-// // eventEmitterObj.removeListener('msg',()=>{
-// //     console.log('removed listener for msg');
-// // });
+// eventEmitterObj.removeListener('msg',()=>{
+//     console.log('removed listener for msg');
+// });
 
-// eventEmitterObj.emit('msg','hii msg event 2...');
-// // eventEmitterObj.removeAllListeners('msg');
-// eventEmitterObj.emit('msg','hii msg event 3...');
-// eventEmitterObj.emit('msg','hii msg event 4...');
-// eventEmitterObj.emit('msg','hii msg event 5...');
+eventEmitterObj.emit('msg','hii msg event 2...');
+// eventEmitterObj.removeAllListeners('msg');
+eventEmitterObj.emit('msg','hii msg event 3...');
+eventEmitterObj.emit('msg','hii msg event 4...');
+eventEmitterObj.emit('msg','hii msg event 5...');
 
-// console.log(eventEmitterObj.listeners('msg'))
+console.log(eventEmitterObj.listeners('msg'))
 
 
 /*......................Asynchronous error.......................*/
 
-// import EventEmitter from 'events';
-// const errorEventEmitter=new EventEmitter();
+import EventEmitter from 'events';
+const errorEventEmitter=new EventEmitter();
 
-// errorEventEmitter.on('error',(err)=>{
-//     console.error('An error occurred:',err.message);
-// });
-// //1
-// try{
-//     const abc=10;
-//     console.log(abc++);
-// }
-// catch(err){
-//     errorEventEmitter.emit('error',err);
-// }
-// console.log('Program execution completed');
+errorEventEmitter.on('error',(err)=>{
+    console.error('An error occurred:',err.message);
+});
+//1
+try{
+    const abc=10;
+    console.log(abc++);
+}
+catch(err){
+    errorEventEmitter.emit('error',err);
+}
+console.log('Program execution completed');
 
-// //2
-// function fun(task){
-//     if (task === 'fail') {
-//           errorEventEmitter.emit('error', new Error('Task failed'));
-//     } else{
-//           console.log('Task performed successfully');
-//     }
-// }
-// fun('fail')
-// fun('completed')
+//2
+function fun(task){
+    if (task === 'fail') {
+          errorEventEmitter.emit('error', new Error('Task failed'));
+    } else{
+          console.log('Task performed successfully');
+    }
+}
+fun('fail')
+fun('completed')
 
 
 /*......................Logging System ....................*/
 
-import EventEmitter from 'events';
+const EventEmitter=require('events');
 
 class Logger extends EventEmitter {
     logInfo(message){
@@ -235,8 +235,138 @@ chatRoom.sendMessage('jack',"hii,i'm jack");
 chatRoom.deleteChat("jack","hii");
 
 
+/*.............................Handling Asynchronous Events......................................................*/
+
+const EventEmitter = require("events");
+const eventEmitter = new EventEmitter();
+
+const asyncHandler = async (message) => {
+    console.log("started...");
+    await new Promise((resolve) =>{
+         setTimeout(()=>{
+             console.log("Async operation started!");
+            console.log(`Received message: ${message}`);
+             resolve();
+         },1000)
+        });
+    console.log("completed!");
+  }
+  eventEmitter.on('data', async function(){
+    try{
+        console.log("Async event handler started!");
+        await asyncHandler("Hello, this is an async event!");
+        console.log("Async event handler completed!");
+    }
+    catch(err){
+        console.error("An error occurred:",err.message);
+    }
+  });
+eventEmitter.emit("data");
 
 
+/*...................................Server Monitoring.......................................................*/
+
+const os = require('os');
+const EventEmitter = require('events');
+class ServerMonitor extends EventEmitter {
+    constructor() {
+    super();
+    this.checkServerLoad();
+    this.checkMemoryUsage();
+    }
+    checkServerLoad() {
+        setInterval(() => {
+            const load = os.loadavg()[0];
+            if (load > 1.5) {
+                this.emit('overload', load);
+            }}, 1000);
+        }
+    checkMemoryUsage(){
+        setInterval(() => {
+            const memUsage = os.totalmem() - os.freemem();
+            const percentage = (memUsage / os.totalmem()) * 100;
+            if (percentage > 80) {
+                this.emit('memory-warning', percentage);
+            }}, 1000);
+    
+    }
+}
+const monitor = new ServerMonitor();
+monitor.on('overload', (load) => {
+console.warn(`Warning: Server overload detected! Load: ${load}`);
+});
+
+monitor.on('memory-warning', (percentage) => {
+console.warn(`Warning: Memory usage is high! Percentage: ${percentage}%`);
+});
 
 
+/*..................................Memory leaks in event emitter...........................................*/
+
+const EventEmitter = require('events');
+const eventEmitterObj=new EventEmitter();
+
+eventEmitterObj.setMaxListeners(15);
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
+
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
+
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
+
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
+
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
+
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
+
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
+
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
+
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
+
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
+
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
+
+eventEmitterObj.addListener('msg',(data)=>{
+    console.log(data);
+})
+
+//avoiding memory leaks
+eventEmitterObj.once('start',(data)=>{
+    console.log(data);
+})
+
+eventEmitterObj.removeAllListeners('msg');
+
+eventEmitterObj.emit('msg','msg event 1');
+eventEmitterObj.emit('start','start event 1');
+eventEmitterObj.emit('start','start event 2');
+eventEmitterObj.emit('start','start event 3');
 
